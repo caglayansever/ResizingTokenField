ba22044694428d5c1ca409b29546c8326f251d1b
Caglayan Sever <caglayan.sever@turkcell.com.tr>
2024-01-23 13:49:53 +0300

    fix: adding textfield to contentview instead of superview

--- ResizingTokenField/Classes/Items/TextFieldCell.swift
+++ ResizingTokenField/Classes/Items/TextFieldCell.swift
@@ -39,7 +39,7 @@ class TextFieldCell: UICollectionViewCell {
     }
     
     private func setUp() {
-        addSubview(textField)
+        contentView.addSubview(textField)
         
         if #available(iOS 10.0, *) {
             textField.adjustsFontForContentSizeCategory = true
